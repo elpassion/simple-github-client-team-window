e4bea7b14e99161ea65ebe696c22607a2cf1251a
jasiekpor <jasiekpor@gmail.com>
2016-01-22 10:55:09 +0100

    cleaning code

--- app/src/main/java/com/example/kit/githubclient/adapter/PlaceItemAdapter.kt
+++ app/src/main/java/com/example/kit/githubclient/adapter/PlaceItemAdapter.kt
@@ -1,40 +1,35 @@
 package pl.elpassion.dmalantowicz.rest_client_example.adapter
 
+
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import com.bumptech.glide.Glide
-import pl.elpassion.dmalantowicz.rest_client_example.R
-import pl.elpassion.dmalantowicz.rest_client_example.domain.Place
 
 /**
  * Created by dmalantowicz on 15.01.2016.
  */
-class PlaceItemAdapter(private val place: Place) : ItemAdapter {
-
-    override val itemViewType = R.id.name_rate_layout
-
-    override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.rest_response_list_item, parent, false)
-        return NameRateItemHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder) {
-        val nameRateItemHolder = holder as NameRateItemHolder
-        Glide.with(nameRateItemHolder.icon.context)
-                .load(place.icon)
-                .into(nameRateItemHolder.icon)
-        nameRateItemHolder.name.text = place.name
-        nameRateItemHolder.rate.text = if (place.rating != null ) place.rating.toString() else ""
-    }
-
-    private inner class NameRateItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val name = itemView.findViewById(R.id.name) as TextView
-        val rate = itemView.findViewById(R.id.rate) as TextView
-        val icon = itemView.findViewById(R.id.placeIcon) as ImageView
-    }
-}
+//class PlaceItemAdapter(private val place: String) : ItemAdapter {
+//
+//    //override val itemViewType = R.id.name_rate_layout
+//
+//    override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
+////        val layoutInflater = LayoutInflater.from(parent.context)
+////        val view = layoutInflater.inflate(R.layout.rest_response_list_item, parent, false)
+//        return NameRateItemHolder()
+//    }
+//
+//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder) {
+////        val nameRateItemHolder = holder as NameRateItemHolder
+////        Glide.with(nameRateItemHolder.icon.context)
+////                .load(place.icon)
+////                .into(nameRateItemHolder.icon)
+////        nameRateItemHolder.name.text = place.name
+////        nameRateItemHolder.rate.text = if (place.rating != null ) place.rating.toString() else ""
+//    }
+//
+//    private inner class NameRateItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+////        val name = itemView.findViewById(R.id.name) as TextView
+////        val rate = itemView.findViewById(R.id.rate) as TextView
+////        val icon = itemView.findViewById(R.id.placeIcon) as ImageView
+//    }
+//}
--- app/src/main/java/com/example/kit/githubclient/adapter/PlacesListAdapter.kt
+++ app/src/main/java/com/example/kit/githubclient/adapter/PlacesListAdapter.kt
@@ -1,15 +1,14 @@
 package pl.elpassion.dmalantowicz.rest_client_example.adapter
 
-import pl.elpassion.dmalantowicz.rest_client_example.domain.Place
 
 /**
  * Created by dmalantowicz on 15.01.2016.
  */
-class PlacesListAdapter(val places : List<Place> ) : BaseAdapter(){
-
-    init{
-        places.forEach {
-            adapters.add(PlaceItemAdapter( it ))
-        }
-    }
-}
+//class PlacesListAdapter(val places : List<String> ) : BaseAdapter(){
+//
+//    init{
+//        places.forEach {
+//            adapters.add(PlaceItemAdapter( it ))
+//        }
+//    }
+//}
